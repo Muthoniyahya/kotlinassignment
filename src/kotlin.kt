2a3fe51870b43ef3ee81f8779de0295bc0d8ceda
student <Muthoniyahya@gmail.com>
2021-02-28 18:02:15 +0300

    first commit

---
+++ src/kotlin.kt
@@ -0,0 +1,24 @@
+fun main() {
+    var name:String=("Hello Iman")
+    println(name)
+    var result=modulus(50,3)
+    println(result)
+
+    var Result=addition(120,180,200,250)
+    println(Result)
+
+    var swimming:Boolean=true
+    println(swimming)
+
+
+}
+fun modulus(num1:Int,num2:Int) :Int{
+    var modulus=num1%num2
+    return modulus
+
+}
+fun addition(Numb1:Int,Numb2:Int,Numb3:Int,Numb4:Int) :Int{
+    var addition=Numb1+Numb2+Numb3+Numb4
+    return addition
+
+}
